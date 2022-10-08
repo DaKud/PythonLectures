2e140a4b88e1e389408902423e6cb7fcf90216a3
DaKu <do.holyada@gmail.com>
2022-10-08 14:59:21 +0300

    a,r,w

--- lec2.py
+++ lec2.py
@@ -1,14 +1,27 @@
 #FILES
 # a - открытие и добавление данных
 # r - открытие для чтения данных
-# w - открыие для записи данных
+# w - открытие для записи данных
 # w+, r+
 
-# A mode
-colors = ['red','green','blue'] 
-data = open('file.txt', 'a') # дозапись
-data.writelies(colors) # разделителей нет
-data.close()
-#R mode
+#VARIANT 1
+# colors = ['red','green','blue123'] 
+# data = open('file.txt', 'a') # a- дозапись w- перезапись
+# # data.writelines(colors) # разделителей нет
+# data.write('\nLINE 12\n') # \n переоход перед записью
+# data.write('LINE 13\n') # \n переход после записи
+# data.close()
 
-#W mode
+#VARIANT 2
+# with open ('file.txt', 'a') as data:
+#     data.write ('line 111111\n')
+#     data.write ('line 33332\n')
+
+#DATA READ
+# path = 'file.txt'
+# data = open(path, 'r') # r- режим чтения
+# for line in data:# цикл бежит по файлу
+#     print(line) # читаем строки
+# data.close
+
+
