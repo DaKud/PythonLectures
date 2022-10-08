28392030614febbff9a0104a13155b17fd16ecbd
DaKu <do.holyada@gmail.com>
2022-10-08 16:36:50 +0300

    SET METHODS

--- lec2.py
+++ lec2.py
@@ -130,3 +130,19 @@
 # colors.clear() # {}
 # print(colors) #set()
 
+#SET METHODS
+a = {1,2,3,5,8}
+b = {2,5,8,13,21}
+c = a.copy() # c= {1,2,3,5,8} - МНОЖНСТВО НА ОСНОВЕ ИМЕЮЩЕГОСЯ
+u = a.union(b) # u = {1,2,3,5,8,13,21} - ОБЪЕДИНЕНИЕ МНОЖНСТВ
+i = a.intersection(b) #i = {8,2,5} - ПЕРЕСЕЧЕНИЕ
+dl = a.difference(b) #dl = {1,3} - РАЗНОСТЬ
+dr = b.difference(a) #dr = {13,21} - РАЗНОСТЬ
+
+q = a \
+    .union(b)\
+    .difference(a.intersection(b))
+#{1,21,3,13}
+
+s = frozenset(a) # неизменяемый тип множества, никакие методы добавления /удаления НЕ работают
+
